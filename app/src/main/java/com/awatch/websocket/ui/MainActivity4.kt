a89dcbdaf685000a7e8fe2c7b2e950956043cad0
文强 <674299693@qq.com>
2025-02-27 17:37:55 +0800

    add more activity

---
+++ app/src/main/java/com/awatch/websocket/ui/MainActivity4.kt
@@ -0,0 +1,12 @@
+package com.awatch.websocket.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.awatch.websocket.R
+
+class MainActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
---
+++ app/src/main/java/com/awatch/websocket/ui/order/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.awatch.websocket.ui.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.awatch.websocket.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main5)
+    }
+}
