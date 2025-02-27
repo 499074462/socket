f8099d95c99fd16818eaff67832d067258057dcd
文强 <674299693@qq.com>
2025-02-27 17:39:09 +0800

    add new Momo

---
+++ app/src/main/java/com/awatch/websocket/ui/Momo.kt
@@ -0,0 +1,12 @@
+git package com.awatch.websocket.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.awatch.websocket.R
+
+class Momo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_momo)
+    }
+}
